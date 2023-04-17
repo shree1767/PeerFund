e96edf39f07415d2a2670f78fbf628bee041e6a7
Paavan Agrawal <33314332+AgrPaavan@users.noreply.github.com>
2023-04-17 18:02:31 +0530

    Initialize repository

---
+++ web3/hardhat.config.js
@@ -0,0 +1,53 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+require("hardhat-deploy");
+
+require("dotenv").config();
+
+const SEPOLIA_RPC_URL =
+  process.env.SEPOLIA_RPC_URL ||
+  "https://eth-sepolia.g.alchemy.com/v2/your-api-key";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  defaultNetwork: "hardhat",
+  // solidity: "0.8.8",
+  solidity: {
+    compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
+  },
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+      blockConfirmations: 6,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: false,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "INR",
+    // coinmarketcap: COINMARKETCAP_API_KEY,
+    // Ethereum -> ETH, Binance -> BNB, Polygon -> MATIC, Avalanche -> AVAX, Heco -> HT, Moonriver -> MOVR
+    token: "ETH",
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+      1: 0,
+    },
+  },
+};
---
+++ web3/helper-hardhat-config.js
@@ -0,0 +1,5 @@
+const developmnetChains = ["hardhat", "localhost"];
+const DECIMALS = 8;
+const INITIAL_ANSWER = 200000000000;
+
+module.exports = { developmnetChains, DECIMALS, INITIAL_ANSWER };
